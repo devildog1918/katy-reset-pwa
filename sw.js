8ee4afd9f15656da5b7f533197b040c6e01e54c4
devildog1918 <robert.perryman27@gmail.com>
2026-03-05 12:05:32 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,7 @@
-const CACHE_NAME = "katy-reset-pwa-v1";
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  event.respondWith(
-    caches.match(req).then((cached) => cached || fetch(req).catch(() => caches.match("./")))
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
