0c182af1f451fab70586796b571d15cbcefed0a5
devildog1918 <robert.perryman27@gmail.com>
2026-03-05 08:50:31 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
+const CACHE = "katy-reset-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
