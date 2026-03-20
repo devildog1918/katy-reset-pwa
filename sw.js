d5ea44951a8a23a2400104bb5a5f3a8035fb36bc
devildog1918 <robert.perryman27@gmail.com>
2026-03-20 14:12:22 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "katy-reset-v5";
+const CACHE_NAME = "katy-reset-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -18,7 +18,11 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(
-        keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null)
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
       )
     )
   );
